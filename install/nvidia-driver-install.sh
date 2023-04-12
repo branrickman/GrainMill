cc378947ce5b1f24fb1cf3eeed6e1f7ac2eb0499
Bryan Brickman <61772752+branrickman@users.noreply.github.com>
2023-04-12 17:22:55 -0500

    modifies nvidia driver installation script
    Uses autoinstall now instead of steps suggested in hashcat article

--- install/nvidia-driver-install.sh
+++ install/nvidia-driver-install.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# NVidia driver setup
-sudo apt install xserver-xorg-core xdm libgtk-3-0
+## The hashcat "Building a Password Cracking Rig pt. III" article recommended the commented out steps below. Instead I used an autoinstaller, since these instructions were not working.
 
-echo "blacklist nouveau options nouveau modeset=0" | sudo tee /etc/modprobe.d/disable-nouveau.conf
-sudo update-initramfs -u
 
-curl -o NVIDIA-Linux-x86_64-384.59.run http://us.download.nvidia.com/XFree86/Linux-x86_64/384.59/NVIDIA-Linux-x86_64-384.59.run
+## NVidia driver setup
+#sudo apt install xserver-xorg-core xdm libgtk-3-0
+#
+#echo "blacklist nouveau options nouveau modeset=0" | sudo tee /etc/modprobe.d/disable-nouveau.conf
+#sudo update-initramfs -u
+#
+#curl -o NVIDIA-Linux-x86_64-384.59.run http://us.download.nvidia.com/XFree86/Linux-x86_64/384.59/NVIDIA-Linux-x86_64-384.59.run
+#
+#sudo bash NVIDIA-Linux-x86_64-384.59.run
+#sudo sed -i '/Driver/a     Option         "Interactive" "False"' /etc/X11/xorg.conf
+#echo 'export DISPLAY=:0' >> ~/.bashrc
+#
+#echo 'Reboot to finalize installation'
 
-sudo bash NVIDIA-Linux-x86_64-384.59.run
-sudo sed -i '/Driver/a     Option         "Interactive" "False"' /etc/X11/xorg.conf
-echo 'export DISPLAY=:0' >> ~/.bashrc
+sudo ubuntu-drivers autoinstall
 
-echo 'Reboot to finalize installation'
+echo 'Reboot to finalize NVIDIA driver installation'
